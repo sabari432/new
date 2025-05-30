4ad93fe64a6c074d0084c1c2c20223aab9f04e53
sabareeswaran N <132356956+sabari432@users.noreply.github.com>
2025-05-30 19:58:02 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 // src/config.js
-const API_BASE_URL1 = 'http://54.89.23.219';
-const API_BASE_URL2 = ' http://54.89.23.219';
-const API_BASE_URL_LOCAL_HOST = 'http://54.89.23.219';
+const API_BASE_URL1 = 'https://54.89.23.219';
+const API_BASE_URL2 = ' https://54.89.23.219';
+const API_BASE_URL_LOCAL_HOST = 'https://54.89.23.219';
 
 export { API_BASE_URL1, API_BASE_URL2, API_BASE_URL_LOCAL_HOST };
 
